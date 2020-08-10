a73017e6e59a3ae36850b891d6b64eceaa31a531
tusimegod123 <43960999+tusimegod123@users.noreply.github.com>
2020-08-10 15:50:41 +0300

    Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.css'
 import Login from './components/login'
 import SignUp from './components/signup'
+import Activities from "./component/Activities";
+import Customers from "./component/Customers";
+import ExpenditurePieChart from "./component/ExpenditurePieChart";
+import SalesBarGraph from "./component/SalesBarGraph";
+import "../css/App.css";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className="App">
+       
           <Switch>
+            
             <Route path="/" exact component={Login} />
             <Route path="/signup" component={SignUp} />
-          </Switch>
-        </div>
+            <ExpenditurePieChart />
+        {/* <SalesBarGraph/> */}
+       
+          {/* <Route path="/" component={Dashboard} exact></Route> */}
+          {/* <Route path="/piechart" component={ExpenditurePieChart} exact /> */}
+          {/* <Route path="/bargraph" component={SalesBarGraph} /> */}
+          <Route path="/customers" component={Customers} exact></Route>
+          <Route path="/activities" component={Activities} exact></Route>
+        </Switch >
+    
       </Router>
     </div>
   );
