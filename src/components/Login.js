07da4b3d3b3001cdfe2d8a7c560563eb61cc7981
Wamala-Emmanuel <kisaakye.emma@gmail.com>
2020-11-02 13:44:58 +0300

    Adding merged code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
-import Divider from '@material-ui/core/Divider';
-import Logo from '../static/images/tree.svg'
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+import Divider from "@material-ui/core/Divider";
+import Logo from "../static/images/tree.svg";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 import axios from "axios";
-
 
 const Buttonn = withStyles({
   root: {
-    '&:hover': {
-      backgroundColor: 'green',
-      opacity: '0.9'
+    "&:hover": {
+      backgroundColor: "green",
+      opacity: "0.9",
     },
-    '&:active , &:focus': {
-      outline: 'none',
+    "&:active , &:focus": {
+      outline: "none",
     },
   },
 })(Button);
 
 const CssTextField = withStyles({
   root: {
-    '& label.Mui-focused': {
-      color: 'rgba(0,0,0,0.87)',
+    "& label.Mui-focused": {
+      color: "rgba(0,0,0,0.87)",
     },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: 'rgba(0, 0, 0, 0.3)',
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "rgba(0, 0, 0, 0.3)",
       },
-      '&.Mui-focused fieldset': {
-        borderColor: 'green',
+      "&.Mui-focused fieldset": {
+        borderColor: "green",
       },
-      '&.Mui-error fieldset': {
-        borderColor: 'red',
+      "&.Mui-error fieldset": {
+        borderColor: "red",
       },
-
     },
   },
 })(TextField);
@@ -88,7 +86,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
   },
   submit: {
-    '&:hover': {
+    "&:hover": {
       color: "white",
     },
     margin: theme.spacing(3, 0, 2),
@@ -103,7 +101,7 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "1rem",
   },
   links: {
-    '&:hover': {
+    "&:hover": {
       color: "green",
     },
     color: "green",
@@ -119,75 +117,87 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: "Segoe UI",
     fontWeight: "800",
     fontSize: "0.9rem",
-  }
+  },
 }));
 
-export default function LogIn() {
+export default function Login() {
   let history = useHistory();
   const classes = useStyles();
-  let [[emaile, passworde], setErrors] = useState(["", ""])
+  let [[emaile, passworde], setErrors] = useState(["", ""]);
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     const url = "http://www.something.com/login";
     let { email, password } = event.target;
 
-    if (email.value !== "" && password.value !== "" && emaile === "" && passworde === "") {
-      axios.post(url, { email, password })
-        .then(data => {
+    if (
+      email.value !== "" &&
+      password.value !== "" &&
+      emaile === "" &&
+      passworde === ""
+    ) {
+      axios
+        .post(url, { email, password })
+        .then((data) => {
           if (data.message === "true") {
-            history.pushState("/projects")
+            history.pushState("/projects");
           } else {
             // Render error message on Login page
           }
         })
         .catch(() => {
           history.push("/pagenotfound");
-        })
+        });
     } else {
-      if (email.value === "" && password.value === "" && emaile === "" && passworde === "") {
-        setErrors(["Enter valid email E.g: abc@gmail.com", "Password should be more than 6 characters"])
-      }
-      else if (email.value === "" && emaile === "" && passworde === "") {
-        setErrors(["Enter valid email E.g: abc@gmail.com", passworde])
-      }
-      else if (password.value === "" && emaile === "" && passworde === "") {
-        setErrors([emaile, "Password should be more than 6 characters",])
+      if (
+        email.value === "" &&
+        password.value === "" &&
+        emaile === "" &&
+        passworde === ""
+      ) {
+        setErrors([
+          "Enter valid email E.g: abc@gmail.com",
+          "Password should be more than 6 characters",
+        ]);
+      } else if (email.value === "" && emaile === "" && passworde === "") {
+        setErrors(["Enter valid email E.g: abc@gmail.com", passworde]);
+      } else if (password.value === "" && emaile === "" && passworde === "") {
+        setErrors([emaile, "Password should be more than 6 characters"]);
       }
       event.preventDefault();
     }
-  }
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    const emailRegex = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    const emailRegex = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
     switch (name) {
       case "email":
-        (!emailRegex.test(value))
+        !emailRegex.test(value)
           ? setErrors(["Enter valid email E.g: abc@gmail.com", passworde])
           : setErrors(["", passworde]);
         break;
 
       case "password":
-        (value.length < 6) ? setErrors([emaile, "Password should be more than 6 characters"]) : setErrors([emaile, ""]);
+        value.length < 6
+          ? setErrors([emaile, "Password should be more than 6 characters"])
+          : setErrors([emaile, ""]);
         break;
 
       default:
         break;
     }
-  }
+  };
 
   useEffect(() => {
-    document.title = "Log In"
+    document.title = "Log In";
   }, []);
-
 
   return (
     <div className={classes.root}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
-
           <div className={classes.titleDiv}>
             <img src={Logo} alt="logo" width="25px" height="25px" />
             <h4 className={classes.navBrand}>Tele-Farmer</h4>
@@ -197,8 +207,13 @@ export default function LogIn() {
             Log In
           </Typography>
 
-          <Typography component="h2" variant="h5" className={classes.pageSubTitle}>
-            Enter your information to stay connected and monitor your project(s) performance.
+          <Typography
+            component="h2"
+            variant="h5"
+            className={classes.pageSubTitle}
+          >
+            Enter your information to stay connected and monitor your project(s)
+            performance.
           </Typography>
 
           <form onSubmit={handleSubmit} className={classes.form} noValidate>
@@ -215,14 +230,18 @@ export default function LogIn() {
               autoComplete="email"
               onChange={handleChange}
             />
-            {emaile && <small
-              style={{
-                color: "red", fontSize: "0.8rem", fontFamily: "Segoe UI"
-              }}
-            >
-              <ErrorOutlineIcon style={{ transform: "scale(0.7)", }} />
-              {emaile}
-            </small>}
+            {emaile && (
+              <small
+                style={{
+                  color: "red",
+                  fontSize: "0.8rem",
+                  fontFamily: "Segoe UI",
+                }}
+              >
+                <ErrorOutlineIcon style={{ transform: "scale(0.7)" }} />
+                {emaile}
+              </small>
+            )}
 
             <CssTextField
               variant="outlined"
@@ -236,17 +255,19 @@ export default function LogIn() {
               error={passworde.length > 0}
               type="password"
               onChange={handleChange}
-
-
             />
-            {passworde && <small
-              style={{ color: "red", fontSize: "0.8rem", fontFamily: "Segoe UI" }}
-            >
-              <ErrorOutlineIcon style={{ transform: "scale(0.7)", }} />
-              {passworde}
-            </small>}
-
-
+            {passworde && (
+              <small
+                style={{
+                  color: "red",
+                  fontSize: "0.8rem",
+                  fontFamily: "Segoe UI",
+                }}
+              >
+                <ErrorOutlineIcon style={{ transform: "scale(0.7)" }} />
+                {passworde}
+              </small>
+            )}
 
             <Link
               to={"/forgotpassword"}
@@ -255,9 +276,7 @@ export default function LogIn() {
               style={{ float: "right" }}
             >
               Forgot password?
-              </Link>
-
-
+            </Link>
 
             <Buttonn
               type="submit"
@@ -265,8 +284,8 @@ export default function LogIn() {
               variant="contained"
               className={classes.submit}
               endIcon={<ArrowRightAltIcon />}
-            component={Link}      // "Component" and "to" prop to be removed.
-            to={"/projects"}     //  Currently for demonstration purposes only.
+              component={Link} // "Component" and "to" prop to be removed.
+              to={"/projects"} //  Currently for demonstration purposes only.
             >
               Login
             </Buttonn>
@@ -274,23 +293,19 @@ export default function LogIn() {
             <Divider />
 
             <Grid container>
-
-              <Grid item xs={12} md={12} lg={12} style={{ textAlign: "center", marginTop: "8px" }}>
-
-                <Link
-                  to={"/signup"}
-                  variant="body2"
-                  className={classes.links}
-                >
+              <Grid
+                item
+                xs={12}
+                md={12}
+                lg={12}
+                style={{ textAlign: "center", marginTop: "8px" }}
+              >
+                <Link to={"/signup"} variant="body2" className={classes.links}>
                   Create account
                 </Link>
-
               </Grid>
-
             </Grid>
-
           </form>
-
         </div>
       </Container>
 
@@ -299,7 +314,6 @@ export default function LogIn() {
           Copyright © {new Date().getFullYear()}&nbsp;| Refactory, Uganda.
         </footer>
       </Container>
-    </div >
-
+    </div>
   );
 }
